a082d1a84678a23fce7e4a0cd37fa3a6ca59a042
Shivmohan <nshivmohan08@gmail.com>
2023-08-17 17:01:43 +0530

    Added global exception handler for InterruptedException

--- client-service/src/main/java/com/shiv/grpc/exception/GlobalExceptionHandler.java
+++ client-service/src/main/java/com/shiv/grpc/exception/GlobalExceptionHandler.java
@@ -1,2 +1,18 @@
-package com.shiv.grpc.exception;public class GlobalExceptionHandler {
+package com.shiv.grpc.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InterruptedException.class)
+    public ResponseEntity<?> handleInterruptedException(InterruptedException interruptedException){
+        log.error(interruptedException.toString());
+        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(interruptedException.getMessage());
+    }
 }
